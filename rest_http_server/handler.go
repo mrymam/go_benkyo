618421ec79b54eede96475c2a6d6ab31b2cd49fb
onyanko-pon <engineer.tsukasa@gmail.com>
2022-04-15 21:46:15 +0900

    [add] article endpoint

--- rest_http_server/handler.go
+++ rest_http_server/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -14,23 +15,37 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		getArticle(w, r)
-		break
 	case "POST":
 		postArticle(w, r)
-		break
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
 func getArticle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	article := Article{
 		Title: "title",
 	}
-	s, _ := json.Marshal(article)
+	articles := []Article{article}
+	s, _ := json.Marshal(articles)
 	fmt.Fprintf(w, string(s))
 }
 
 func postArticle(w http.ResponseWriter, r *http.Request) {
+	article := Article{}
 
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	jsonString := buf.String()
+	err := json.Unmarshal([]byte(jsonString), &article)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	s, _ := json.Marshal(article)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, string(s))
 }
