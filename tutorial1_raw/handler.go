d166e1b7be80ce618577a894cb04c99e1151c89d
onyanko-pon <engineer.tsukasa@gmail.com>
2022-04-16 02:47:02 +0900

    [fix] json from struct

--- tutorial1_raw/handler.go
+++ tutorial1_raw/handler.go
@@ -6,9 +6,20 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	m := map[string]string{"message": "hello"}
-	s, _ := json.Marshal(m)
+
+	res := Response{
+		Message: "hello",
+	}
+	s, err := json.Marshal(&res)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, string(s))
 }
