42d54c8fb9232740deb93ba28b2f379c692e56f7
onyanko-pon <engineer.tsukasa@gmail.com>
2022-04-17 02:48:21 +0900

    [add] mysql container and connect

--- tutorial2/cmd/router.go
+++ tutorial2/cmd/router.go
@@ -12,7 +12,7 @@ import (
 )
 
 func connectDB() (gorm.DB, error) {
-	dsn := "admin:password@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "admin:password@tcp(sample-mysql:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	return *db, err
